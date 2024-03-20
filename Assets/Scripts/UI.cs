a92e130890cf671a97d632b19b84a15c65cf0e98
harrisonsscott <harrisonsterlingscott@gmail.com>
2024-03-19 17:03:02 -0700

    Added a transition function

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -1,8 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+// handles all the UI functions
 public class UI : MonoBehaviour
 {
+    [Header("GameObjects")]
+    public GameObject newGameButton;
+    public GameObject homeScene;
+    public GameObject newGameScene;
+    [Header("Other")]
+    const float transitionTime = 0.1f;
+    private void Start() {
+        // adding listeners to buttons
+        newGameButton.GetComponent<Button>().onClick.AddListener(() => TransitionScene(homeScene, newGameScene));
+    }
+    public void TransitionScene(GameObject from, GameObject to){ // moves one scene off the screen and moves another to it
+        RectTransform fromRect = from.GetComponent<RectTransform>();
+        RectTransform toRect = to.GetComponent<RectTransform>();
 
+        LeanTween.moveLocal(from, new Vector3(-fromRect.rect.width, 0, 0), transitionTime).setOnComplete(() => {
+            from.SetActive(false);
+        });
+
+        to.SetActive(true);
+        toRect.localPosition = new Vector3(toRect.rect.width, 0, 0);
+        LeanTween.moveLocal(to, Vector3.zero, transitionTime);
+    }
 }
