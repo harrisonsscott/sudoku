307bb0a189e38ead53d1b7d045a419c5dad1f68c
harrisonsscott <harrisonsterlingscott@gmail.com>
2024-04-18 10:49:01 -0700

    Prepared for production

--- Assets/Scripts/Sudoku.cs
+++ Assets/Scripts/Sudoku.cs
@@ -28,7 +28,7 @@ public class Sudoku : MonoBehaviour, IPointerClickHandler
         image.material = highlightMaterial;
 
         highlightX.transform.parent = FindAnyObjectByType<Sudoku>().transform.parent;
-        highlightX.transform.localPosition = new Vector3(0, -1000, 0);
+        highlightX.transform.localPosition = new Vector3(0, -10000, 0);
         highlightX.transform.localScale = new Vector3(1,1,1);
         highlightX.AddComponent<RectTransform>();
         
